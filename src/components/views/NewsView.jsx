c5ce4887c1fc71d961de8927ad4acc7922b2d078
Alessandro Barreto <alessandrobarreto1201@gmail.com>
2020-08-23 01:47:38 -0300

    add header

---
+++ src/components/views/NewsView.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewsView = () => {
+  return (
+    <section style={{ backgroundColor: "black", height: "100%" }}>
+      <div>hahahahaha</div>
+    </section>
+  );
+};
+
+export default NewsView;
