c5d453337606544d966c1fbf162339828bb4f73f
Alessandro Barreto <alessandrobarreto1201@gmail.com>
2020-08-26 16:57:48 -0300

    Updates

---
+++ precache-manifest.e6c865d84f370f54186906badc471ce4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df849a0d146fdad3278b596b29367f1",
+    "url": "/Aquarela-Challenge/index.html"
+  },
+  {
+    "revision": "0142fb57cad33213af58",
+    "url": "/Aquarela-Challenge/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e211a318aa80a174a710",
+    "url": "/Aquarela-Challenge/static/css/main.9dad3098.chunk.css"
+  },
+  {
+    "revision": "0142fb57cad33213af58",
+    "url": "/Aquarela-Challenge/static/js/2.3ae9fcc0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Aquarela-Challenge/static/js/2.3ae9fcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e211a318aa80a174a710",
+    "url": "/Aquarela-Challenge/static/js/main.5dadc037.chunk.js"
+  },
+  {
+    "revision": "29494410021770ba702f",
+    "url": "/Aquarela-Challenge/static/js/runtime-main.57ce8f09.js"
+  },
+  {
+    "revision": "796fbe177e647660ecd544a4ac31d072",
+    "url": "/Aquarela-Challenge/static/media/logo.796fbe17.svg"
+  },
+  {
+    "revision": "316d5f10da774e6154d0b8f2949cded7",
+    "url": "/Aquarela-Challenge/static/media/mainNew.316d5f10.svg"
+  },
+  {
+    "revision": "4843fd259ed72da078ecb77b650b0ce0",
+    "url": "/Aquarela-Challenge/static/media/subNew1.4843fd25.svg"
+  },
+  {
+    "revision": "462b793afd8bf8a829867b3d064052be",
+    "url": "/Aquarela-Challenge/static/media/subNew2.462b793a.svg"
+  },
+  {
+    "revision": "0b775cbb555a0f49243ebfab76bb1364",
+    "url": "/Aquarela-Challenge/static/media/subNew3.0b775cbb.svg"
+  },
+  {
+    "revision": "bfd64ada01a4e33acd4c031df741c3b3",
+    "url": "/Aquarela-Challenge/static/media/subNew4.bfd64ada.svg"
+  },
+  {
+    "revision": "e21a0f66850b7ceb4359ec3e940cc62f",
+    "url": "/Aquarela-Challenge/static/media/subNew5.e21a0f66.svg"
+  },
+  {
+    "revision": "2adba730555c305ea1e006cacc4c479b",
+    "url": "/Aquarela-Challenge/static/media/subNew6.2adba730.svg"
+  }
+]);
