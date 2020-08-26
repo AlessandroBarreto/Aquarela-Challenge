678cd22d61314f917497a9ca0df7800849050a5f
Alessandro Barreto <alessandrobarreto1201@gmail.com>
2020-08-26 15:44:07 -0300

    class organization

--- src/components/MainNew/index.jsx
+++ src/components/MainNew/index.jsx
@@ -4,26 +4,26 @@ import { Row, Col, Card } from "react-bootstrap";
 
 const MainNew = (props) => {
   return (
-    <Card className="main-newsCard">
+    <Card className="mainnew-card">
       <Row noGutters={true}>
         <Col xs={12} lg={6}>
-          <img src={props.img} alt="" className="mainNewImage" />
+          <img src={props.img} alt="" className="mainnew-image" />
         </Col>
         <Col xs={12} lg={6}>
-          <Card.Body className="mainNewCardBody">
+          <Card.Body className="mainnew-card-body">
             <p className="mainnew-author-span">
               por {props.author} | {props.date}
             </p>
-            <div className="paragraphsContainer">
+            <div className="paragraphs-container">
               {props.paragraphs.map((paragraph) => (
-                <Card.Text className="mainNewCardText">{paragraph}</Card.Text>
+                <Card.Text className="mainnew-card-text">{paragraph}</Card.Text>
               ))}
             </div>
             <div className="mainnew-read-more-container">
               <a
                 href={props.link}
                 target="blank"
-                className="mainnew-newsCardButton"
+                className="mainnew-card-button"
               >
                 Ler mais
               </a>
--- src/components/views/NewsView.jsx
+++ src/components/views/NewsView.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React from "./node_modules/react";
 import MainNew from "../MainNew";
 import SubNew from "../SubNew";
 import { subNews, mainNew } from "../../mocks/news";
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row } from "./node_modules/react-bootstrap";
 import "./styles.css";
 
 const NewsView = () => {
   return (
-    <section className="newSection">
+    <section className="new-section">
       <Container fluid>
-        <MainNew img={mainNew.img} paragraphs={mainNew.paragraphs} author={mainNew.author} date={mainNew.date} link={mainNew.readMoreUrl}/>
+        <MainNew
+          img={mainNew.img}
+          paragraphs={mainNew.paragraphs}
+          author={mainNew.author}
+          date={mainNew.date}
+          link={mainNew.readMoreUrl}
+        />
         <Row>
           {subNews.map((subNew) => (
-            <Col md={6} sm={12} className="subNewCol">
-              <SubNew img={subNew.img} description={subNew.description} author={subNew.author} date={subNew.date} link={subNew.readMoreUrl} />
-            </Col> 
+            <Col md={6} sm={12} className="subnew-col">
+              <SubNew
+                img={subNew.img}
+                description={subNew.description}
+                author={subNew.author}
+                date={subNew.date}
+                link={subNew.readMoreUrl}
+              />
+            </Col>
           ))}
         </Row>
       </Container>
