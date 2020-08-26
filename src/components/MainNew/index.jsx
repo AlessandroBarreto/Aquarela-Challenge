3b76f46107fe5b449934d3dc1a3c173a3a2ce74d
Alessandro Barreto <alessandrobarreto1201@gmail.com>
2020-08-26 14:58:38 -0300

    formatting code

--- src/components/MainNew/index.jsx
+++ src/components/MainNew/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import "./styles.css"
+import React from "react";
+import "./styles.css";
 import { Row, Col, Card } from "react-bootstrap";
 
 const MainNew = (props) => {
-  return ( 
+  return (
     <Card className="main-newsCard">
       <Row noGutters={true}>
         <Col xs={12} lg={6}>
@@ -11,22 +11,28 @@ const MainNew = (props) => {
         </Col>
         <Col xs={12} lg={6}>
           <Card.Body className="mainNewCardBody">
-              <p className="mainnew-author-span">
-                por {props.author} | {props.date}
-              </p>
+            <p className="mainnew-author-span">
+              por {props.author} | {props.date}
+            </p>
             <div className="paragraphsContainer">
-            {props.paragraphs.map((paragraph) => (
-              <Card.Text className="mainNewCardText">{paragraph}</Card.Text>
-            ))}
+              {props.paragraphs.map((paragraph) => (
+                <Card.Text className="mainNewCardText">{paragraph}</Card.Text>
+              ))}
             </div>
             <div className="mainnew-read-more-container">
-              <a href={props.link} target="blank" className="mainnew-newsCardButton">Ler mais</a>
+              <a
+                href={props.link}
+                target="blank"
+                className="mainnew-newsCardButton"
+              >
+                Ler mais
+              </a>
             </div>
           </Card.Body>
         </Col>
       </Row>
     </Card>
-  )
-}
+  );
+};
 
 export default MainNew;
--- src/components/SubNew/index.jsx
+++ src/components/SubNew/index.jsx
@@ -4,19 +4,27 @@ import { Row, Col, Card } from "react-bootstrap";
 
 const SubNew = (props) => {
   return (
-    <Card className="subNewsCard">
+    <Card className="subnew-card">
       <Row noGutters={true}>
         <Col xs={12} xl={6}>
-          <img src={props.img} alt="" className="subNewImage" />
+          <img src={props.img} alt="" className="subnew-image" />
         </Col>
         <Col xs={12} xl={6}>
           <Card.Body>
-            <Card.Text className="subnew-cardText">{props.description}</Card.Text>
+            <Card.Text className="subnew-card-text">
+              {props.description}
+            </Card.Text>
             <div className="subnew-read-more-container">
               <span className="subnew-author-span">
                 por {props.author} | {props.date}
               </span>
-              <a href={props.link} target="blank" className="subnew-newsCardButton">Ler mais</a>
+              <a
+                href={props.link}
+                target="blank"
+                className="subnew-card-button"
+              >
+                Ler mais
+              </a>
             </div>
           </Card.Body>
         </Col>
